4da6dc116065eccc1e282fc7b91811e779dfc2ee
Anton Samoilov <absamoilov88@gmail.com>
2021-01-31 17:38:03 +0200

    Connect multiple wizards

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { Composer, Markup, Scenes, session, Telegraf } from "telegraf";
 import hourlyWizard from "./hourlyWizard.js";
+import morningWizard from "./morningWizard.js";
+import eveningWizard from "./eveningWizard.js";
 
 /**
  *
@@ -18,17 +20,26 @@ if (token === undefined) {
  */
 
 const bot = new Telegraf(token);
-const stage = new Scenes.Stage([hourlyWizard]);
+const stage = new Scenes.Stage([hourlyWizard, morningWizard, eveningWizard]);
 
 bot.use(session());
 bot.use(stage.middleware());
 
-bot.command("text", (ctx) => {
+bot.command("morning", (ctx) => {
+    return ctx.scene.enter("morning-wizard");
+});
+bot.command("hourly", (ctx) => {
     return ctx.scene.enter("hourly-wizard");
+});
+bot.command("evening", (ctx) => {
+    return ctx.scene.enter("evening-wizard");
 });
 
 bot.on("text", (ctx) => {
-    ctx.reply("k");
+    ctx.reply(
+        "Choose an option",
+        Markup.keyboard([["/hourly", "/morning", "/evening"]])
+    );
 });
 
 /**
