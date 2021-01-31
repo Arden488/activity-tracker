5bd1ab9ec507ebcf08cb2b27acb1939c639ae2fb
Anton Samoilov <absamoilov88@gmail.com>
2021-02-01 00:04:39 +0200

    Add firestore config

---
+++ firestore.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+// import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAO2wJL1suOGZ3XSGQe_L4v1z9Ezia3sNw",
+    authDomain: "tracker-47ddf.firebaseapp.com",
+    projectId: "tracker-47ddf",
+    storageBucket: "tracker-47ddf.appspot.com",
+    messagingSenderId: "704615672308",
+    appId: "1:704615672308:web:127db1ae7b6084d3d931a1",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+
+export default firebase;
