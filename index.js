579cc443279f7e21f64c7ab336128117aa7cf78d
Anton Samoilov <absamoilov88@gmail.com>
2021-01-31 16:29:38 +0200

    Update webhook config

--- index.js
+++ index.js
@@ -21,18 +21,17 @@ bot.on("text", (ctx) => ctx.reply("asd"));
 // bot.use(session());
 // bot.use(stage.middleware());
 
-process.env.NODE_ENV === "production" ? startProdMode(bot) : startDevMode(bot);
+bot.launch(
+    process.env.NODE_ENV === "production"
+        ? {
+              webhook: {
+                  domain: `${process.env.HEROKU_URL}/${secretPath}`,
+                  port: PORT,
+              },
+          }
+        : {}
+);
 
-function startDevMode(bot) {
-    bot.launch();
-
-    // Enable graceful stop
-    process.once("SIGINT", () => bot.stop("SIGINT"));
-    process.once("SIGTERM", () => bot.stop("SIGTERM"));
-}
-
-function startProdMode(bot) {
-    bot.telegram.setWebhook(`${process.env.HEROKU_URL}/${secretPath}`);
-
-    bot.startWebhook(secretPath, null, PORT);
-}
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
