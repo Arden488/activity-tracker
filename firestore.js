5f7e64316b35f19d494412f5547811c5c07671c2
Anton Samoilov <absamoilov88@gmail.com>
2021-03-02 13:28:39 +0200

    Update certificate data

--- firestore.js
+++ firestore.js
@@ -1,5 +1,5 @@
 import firebase from "firebase";
-import * as admin from "firebase-admin";
+import admin from "firebase-admin";
 // import "firebase/firestore";
 
 const firebaseConfig = {
@@ -9,7 +9,11 @@ const firebaseConfig = {
     storageBucket: "tracker-47ddf.appspot.com",
     messagingSenderId: "704615672308",
     appId: "1:704615672308:web:127db1ae7b6084d3d931a1",
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY,
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
 };
 
 // Initialize Firebase
