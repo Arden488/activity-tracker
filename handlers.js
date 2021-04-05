dff2fb18ab0e9fddf74c52e2f50555443fd01584
Anton Samoilov <absamoilov88@gmail.com>
2021-04-05 19:47:41 +0300

    Disable morning questions

--- handlers.js
+++ handlers.js
@@ -1,23 +1,15 @@
 import { Composer, Markup, Scenes, session, Telegraf } from "telegraf";
 import hourlyWizard from "./hourlyWizard.js";
-import morningWizard from "./morningWizard.js";
 import eveningWizard from "./eveningWizard.js";
 
 export const registerHandlers = (bot) => {
-    const stage = new Scenes.Stage([
-        hourlyWizard,
-        morningWizard,
-        eveningWizard,
-    ]);
+    const stage = new Scenes.Stage([hourlyWizard, eveningWizard]);
 
     bot.use(session());
     bot.use(stage.middleware());
 
     bot.command("morning", (ctx) => {
         return ctx.scene.enter("morning-wizard");
-    });
-    bot.command("hourly", (ctx) => {
-        return ctx.scene.enter("hourly-wizard");
     });
     bot.command("evening", (ctx) => {
         return ctx.scene.enter("evening-wizard");
@@ -26,7 +18,7 @@ export const registerHandlers = (bot) => {
     bot.on("text", (ctx) => {
         ctx.reply(
             "Choose an option",
-            Markup.keyboard([["/hourly", "/morning", "/evening"]])
+            Markup.keyboard([["/hourly", "/evening"]])
         );
     });
 };
